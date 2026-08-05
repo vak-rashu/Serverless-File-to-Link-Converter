05008cf106f4e6576fc0910e8252496d022b6833
vak-rashu <rashu.cloud11@gmail.com>
2026-08-05 16:22:42 +0000

    ->Created Tests for the Modules

--- modules/api-gw/outputs.tf
+++ modules/api-gw/outputs.tf
@@ -5,3 +5,11 @@ output "rest_api_id" {
 output "api_stage_name" {
   value = aws_api_gateway_stage.stage.stage_name
 }
+
+output "api_gateway_short_uri" {
+  value = aws_api_gateway_integration.integration_short.uri
+}
+
+output "api_gateway_shorten_uri" {
+  value = aws_api_gateway_integration.integration_shorten.uri
+}
