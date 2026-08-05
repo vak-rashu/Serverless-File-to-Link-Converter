ec56e7b94f6caf777b376e466f51d0be4f2fbad2
vak-rashu <rashu.cloud11@gmail.com>
2026-08-05 16:23:25 +0000

    Created Tests for the Modules

--- modules/main.tf
+++ modules/main.tf
@@ -1,31 +1,31 @@
 module "s3bucket" {
-  source = "./s3"
+  source                  = "./s3"
   file_upload_bucket_name = "newBucket123"
-  frontend_bucket_name = "frontend_asses_bucket_name"
+  frontend_bucket_name    = "frontend_asses_bucket_name"
 }
 
 module "dynamodb" {
-  source = "./dynamo-db"
+  source              = "./dynamo-db"
   dynamodb_table_name = "newDB123"
 }
 
 module "lambda_func" {
-  source = "./lambda"
-  source_dir = "../myproject/"
-  output_path = "../myproject/function.zip"
-  runtime_type = "python3.12"
+  source        = "./lambda"
+  source_dir    = "../myproject/"
+  output_path   = "../myproject/function.zip"
+  runtime_type  = "python3.12"
   function_name = "my-function123"
 
-  bucket_name = module.s3bucket.file_upload_bucket_name
+  bucket_name       = module.s3bucket.file_upload_bucket_name
   dynamo_table_name = module.dynamodb.dynamo_table_name
-  s3_bucket_arn = module.s3bucket.file_upload_bucket_arn
-  db_table_arn = module.dynamodb.db_table_arn
+  s3_bucket_arn     = module.s3bucket.file_upload_bucket_arn
+  db_table_arn      = module.dynamodb.db_table_arn
 
   rest_api_id = module.api_gw.rest_api_id
 }
 
 module "api_gw" {
-  source = "./api-gw"
-  lambda_func_arn = module.lambda_func.lambda_func_invoke_arn
+  source           = "./api-gw"
+  lambda_func_arn  = module.lambda_func.lambda_func_invoke_arn
   lambda_func_name = module.lambda_func.lambda_func_name
 }
