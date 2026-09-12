2fa87151ba46b6bd6f7b5ae0cea370bbf46842df
Rajeshwari Vakharia <rashu.cloud11@gmail.com>
2026-09-12 22:56:28 +0530

    update s3 to remove for_each block

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,8 +1,16 @@
 #create a bucket resource
+# resource "aws_s3_bucket" "s3bucket"{
+#   for_each = {
+#     file_upload = var.upload_file_bucket_name
+#     frontend_assest = var.frontend_assests_bucket_name
+#   }
+#   bucket = each.value
+# }
+
 resource "aws_s3_bucket" "s3bucket"{
-  for_each = {
-    file_upload = var.upload_file_bucket_name
-    frontend_assest = var.frontend_assests_bucket_name
-  }
-  bucket = each.value
+  bucket = var.upload_file_bucket_name
+}
+
+resource "aws_s3_bucket" "asset_bucket"{
+  bucket = var.frontend_assets_bucket_name
 }
